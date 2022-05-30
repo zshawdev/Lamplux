738c13f958cc69d7803012f322d15fea0300c73a
Zane Shaw <zshawdev@gmail.com>
2022-05-30 10:26:09 -0400

    Clean up codebase

--- simple-style.js
+++ simple-style.js
@@ -1,4 +1,4 @@
-const interview = document.querySelector("#interview");
+const sectionInterview = document.querySelector("#section-interview");
 let articleTextJSON;
 
 /// Interview Generating via JSON
@@ -16,40 +16,40 @@ const generateArticleBlocks = (articleTextJSON) => {
     const max = Object.keys(articleTextJSON).length;
     for (i = 0; i <= max; i++) {
         articleBlock = document.createElement("div");
-        articleBlock.classList.add("interview__article-block");
+        articleBlock.classList.add("section-interview__article-block");
         
         if (articleTextJSON[i].type === "dialogue") {
-            articleBlock.classList.add("interview__article-block--dialogue");
-            interview.append(articleBlock);
+            articleBlock.classList.add("section-interview__article-block--dialogue");
+            sectionInterview.append(articleBlock);
 
             const span = document.createElement("span");
-            span.classList.add("interview__dialogue-author");
+            span.classList.add("section-interview__dialogue-author");
             const author = eval(`articleTextJSON[${i}].en_US.author`);
             span.innerHTML = author;
             articleBlock.append(span);
 
             const p = document.createElement("p");
-            p.classList.add("interview__dialogue-text");
+            p.classList.add("section-interview__dialogue-text");
             const text = eval(`articleTextJSON[${i}].en_US.text`);
             p.innerHTML = text;
             articleBlock.append(p);
         }
 
         else if (articleTextJSON[i].type === "context") {
-            articleBlock.classList.add("interview__article-block--context");
-            interview.append(articleBlock);
+            articleBlock.classList.add("section-interview__article-block--context");
+            sectionInterview.append(articleBlock);
             const span = document.createElement("span");
-            span.classList.add("interview__context-text");
+            span.classList.add("section-interview__context-text");
             const text = eval(`articleTextJSON[${i}].en_US.text`);
             span.innerHTML = text;
             articleBlock.append(span);
         }
 
         else if (articleTextJSON[i].type === "image") {
-            articleBlock.classList.add("interview__article-block--image");
-            interview.append(articleBlock);
+            articleBlock.classList.add("section-interview__article-block--image");
+            sectionInterview.append(articleBlock);
             const img = document.createElement("img");
-            img.classList.add("interview__image");
+            img.classList.add("section-interview__image");
             const srcLink = eval(`articleTextJSON[${i}].srcLink`);
             img.src = srcLink;
             const alt = eval(`articleTextJSON[${i}].alt`);
