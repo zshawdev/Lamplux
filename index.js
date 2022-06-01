73d222636d78896ccb1d2aec08d51da3de4eea69
Zane Shaw <zshawdev@gmail.com>
2022-06-01 15:47:49 -0400

    Create interviews page

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.get("/simple-style", (req, res) => {
     res.sendFile(path.join(__dirname, "/simple-style.html"));
 })
 
-app.get("/simple-style", (req, res) => {
+app.get("/tower-records", (req, res) => {
     res.sendFile(path.join(__dirname, "/tower-records.html"));
 })
 
-app.get("/nav.js", (req, res) => {
-    res.sendFile(path.join(__dirname, "/nav.js"));
+app.get("/interviews", (req, res) => {
+    res.sendFile(path.join(__dirname, "/interviews.html"));
 })
 
 app.get("*", (req, res) => {
